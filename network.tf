f5a084151a9f32f7e9d9525a91f24d11ced2fc9b
haiqing <haiqing0065@gmail.com>
2022-03-11 00:21:23 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/hq-training/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "hqtest"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
